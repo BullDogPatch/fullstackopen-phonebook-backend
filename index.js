0fa89286a39cf2a21d3892fe4ddfa6ad43771dcd
Craig Clayton <craig__clayton@outlook.com>
2024-04-01 22:09:28 +0100

    delete works i think

--- index.js
+++ index.js
@@ -87,9 +87,13 @@ app.get('/api/persons/:id', (request, response) => {
 
 /* delete person at id */
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-  response.status(204).end();
+  // const id = Number(request.params.id);
+  // persons = persons.filter(person => person.id !== id);
+  // response.status(204).end();
+
+  Person.findByIdAndDelete(request.params.id).then(() => {
+    response.status(204).end();
+  });
 });
 
 // const generated = () => {
